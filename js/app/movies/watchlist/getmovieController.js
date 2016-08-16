fd35c87a9e433d435a0765e118bab29162b1c0be
alucardu <peter@no-illusions.nl>
2016-08-16 13:21:11 +0200

    Deploy to Heroku

--- js/app/movies/watchlist/getmovieController.js
+++ js/app/movies/watchlist/getmovieController.js
@@ -80,7 +80,6 @@ angular.module('movieSeat')
         });
 
         $scope.$on('onRemoveMovieEventUpdate', function (event, movie) {
-            console.log('onRemoveMovieEventUpdate!!!! Deploy');
             var index = $scope.moviesX.indexOf(movie);
             $scope.moviesX.splice(index, 1);
 
