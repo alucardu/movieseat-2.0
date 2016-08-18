03392329fdc9e0b99ed05ba9ba89296dc4fd55c8
alucardu <peter@no-illusions.nl>
2016-08-18 22:25:31 +0200

    Added some styling to signIn/signUp, need to create sessions

--- js/min/concat.js
+++ js/min/concat.js
@@ -437,6 +437,7 @@ angular.module('movieSeat')
                     Notification.success('You have logged in');
                 } else {
                     Notification.error('Incorrect username/password combination');
+
                 }
             });
         }
